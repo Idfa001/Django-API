47bfc7585600a7a24ab86214c7e0a2b15d12e03d
Idfa001 <idfa001@gmail.com>
2020-10-05 15:12:21 +0700

    5 0kt 2020

---
+++ 08-01/r/belajar_api_django/task/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse, HttpResponse
+
+def print(request):
+    html = [
+        {
+            'name' : 'Josefine',
+            'moto' : 'The universe has a beginning, but no end'},
+        {
+            'name' : 'Christina',
+            'moto' : 'You can run away, but that\'ll just make it worse!'}
+    ]
+    
+
+    return JsonResponse({'html' : html})
