8c690ed0e0ab6f9aafdab9a2e4a442ab7c1599b2
Idfa001 <idfa001@gmail.com>
2020-10-07 09:26:47 +0700

    7 okt 2020

--- 08-02/belajar_api_django/task/urls.py
+++ 08-02/belajar_api_django/task/urls.py
@@ -7,28 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    # path('create/', views.create),
-    # path('<id>/', views.detail),
-    # path('<id>/update', views.update),
-    # path('<id>/delete', views.delete),
+    path('create/', views.create),
+    path('<id>/', views.detail),
+    path('<id>/update', views.update),
+    path('<id>/delete', views.delete),
 ]
-
-
-
-
-
-
-
-
-
-
-
-# from django.urls import path,include
-
-# from . import views
-
-# urlpatterns = [
-
-#     path ('', views.print),
-
-# ]
--- 08-02/belajar_api_django/task/views.py
+++ 08-02/belajar_api_django/task/views.py
@@ -1,29 +1,66 @@
-from django.http import JsonResponse
-from . import models
-def index(req):
-    data = models.Crud.objects.all() 
+from django.http import HttpResponse, JsonResponse
+import json
+from django.forms.models import model_to_dict
+from .models import Crud
 
-    simpan = [] #ibarat kaleng kosong untuk menyimpan data
-    for e in data: #query list
-        simpan.append({
-            'nama':e.nama, # append untuk menambahkan data
-            'motto':e.motto
-        })
+
+# untuk HttpResponse
+
+# def index (req):
+#     html = "apa aja bileh",;
+#     return HttpResponse(html)
+
+
+# untuk JsonResponse
+
+def index(req):
+    data = Crud.objects.all()  # query set
+
+    simpan = []  # ibarat kaleng kosong untuk menyimpan data
+    for e in data:  # query list
+        simpan.append(model_to_dict(e))
     return JsonResponse({
-        'data': simpan #
+        'data': simpan
     }, safe=False)
 
 
+def create(req):
+    if req.method == 'POST': #untuk mengecek post atau tidak
+        data_byte = req.body #menyimpan req.body ke bentuk byte
+        data_string = str(data_byte, 'utf-8') #merubah req.body ke data string
+        data = json.loads(data_string)# mengirim data string dalam bentuk Json
 
-    # def print(request):
-#     html = [
-#         {
-#             'name' : 'Josefine',
-#             'moto' : 'The universe has a beginning, but no end'},
-#         {
-#             'name' : 'Christina',
-#             'moto' : 'You can run away, but that\'ll just make it worse!'}
-#     ]
-    
+        crud = Crud.objects.create(nama=data['nama'], motto=data['motto'])#create data
+        return JsonResponse({
+            'data': model_to_dict(crud),# model_to_dict berfungsi untuk mengubah model ke dalam dictionary
+            }) 
 
-#     return JsonResponse({'html' : html})
+def delete(req, id):
+    if req.method == 'DELETE':
+        crud = Crud.objects.filter(pk=id).delete()
+    return JsonResponse({
+        'msg':'data has ben delete'
+    })
+
+def detail(req, id):
+    if req.method == 'GET':
+        crud = Crud.objects.filter(pk=id).first()
+        return JsonResponse({
+            'data': model_to_dict(crud),
+        })
+def update(req, id):
+    if req.method == 'PUT':
+
+        data_byte = req.body #menyimpan req.body ke bentuk byte
+        data_string = str(data_byte, 'utf-8') #merubah req.body ke data string
+        data = json.loads(data_string)# mengirim data string dalam bentuk Json
+
+        crud = Crud.objects.filter(pk=id).update(nama=data['nama'], motto=data['motto'])
+        crud = Crud.objects.filter(pk=id).first()
+
+        return JsonResponse({
+            'data': model_to_dict(crud),# model_to_dict berfungsi untuk mengubah model ke dalam dictionary
+            }) 
+        
+
+
